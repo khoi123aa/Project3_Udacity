fcb9bb0ac2a5972204329dfe76aeff803d40198d
Tieu Ha Anh Khoi _ K14 FUG CT <ginkizero123@gmail.com>
2023-06-21 12:20:00 +0700

    update new
    update new

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6f7995b46c1d4873a8fcb2fc78ae765-1643109977.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
